b9141cb24e61da7b6b331726d0f20eb1ca97e7b6
kornukovov <kornukovov@yandex.ru>
2022-07-04 19:16:09 +0300

    dey-1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./components/AppRouter";
+import React from "react";
 
 function App() {
   return (
     <BrowserRouter>
       <AppRouter />
     </BrowserRouter>
+    
   );
 }
 
--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,21 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Redirect } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import { authRoutes, publicRoutes } from "../routes";
 
 
 export const AppRouter = () => {
-   const isAuth = false
+   const isAuth = false;
    return (
-      <Router>
-         <Routes>
-            {isAuth && authRoutes.map(({ path, Component }) =>
-               <Route key={path} path={path} element={Component} />
-            )}
-            {publicRoutes.map(({ path, Component }) =>
-               <Route key={path} path={path} element={Component} />
-            )}
-         </Routes>
-      </Router>
+      <Routes>
+         {isAuth && authRoutes.map(({ path, Component }) =>
+            <Route key={path} path={path} element={<Component />} />
+         )}
+
+         {publicRoutes.map(({ path, Component }) =>
+            <Route key={path} path={path} element={<Component />} />
+         )}
+      </Routes>
 
    )
 }
